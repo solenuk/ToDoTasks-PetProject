34a68eebb27e5d27fa1313b17a144c685fcbd338
Ihor Selianyk <iseljani@gmail.com>
2026-06-09 13:02:16 +0300

    initial commit

---
+++ src/main/java/com/solenuk/todotaskspetproject/ToDoTasksPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.solenuk.todotaskspetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTasksPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoTasksPetProjectApplication.class, args);
+    }
+
+}
