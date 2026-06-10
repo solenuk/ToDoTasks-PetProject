dcb9305afdb3ecede4aa83e798c30e7b5fcbe2de
Ihor Selianyk <iseljani@gmail.com>
2026-06-10 17:43:08 +0300

    Created user entity and configured the checkstyle plugin for this project

---
+++ src/main/java/com/solenuk/todotaskspetproject/entities/User.java
@@ -0,0 +1,47 @@
+package com.solenuk.todotaskspetproject.entities;
+
+import com.solenuk.todotaskspetproject.enums.UserRole;
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 20)
+    private UserRole role;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
---
+++ src/main/java/com/solenuk/todotaskspetproject/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.solenuk.todotaskspetproject.enums;
+
+public enum UserRole {
+    USER_ROLE,
+    ADMIN_ROLE
+}
