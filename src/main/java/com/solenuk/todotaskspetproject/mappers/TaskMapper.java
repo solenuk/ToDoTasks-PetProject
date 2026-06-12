be14578a2aab0aadd57a3f14ecf84d50e19bf640
Ihor Selianyk <166725875+solenuk@users.noreply.github.com>
2026-06-12 12:43:49 +0300

    Created mapper for Task entity and DTOs (#35)

---
+++ src/main/java/com/solenuk/todotaskspetproject/mappers/TaskMapper.java
@@ -0,0 +1,33 @@
+package com.solenuk.todotaskspetproject.mappers;
+
+import com.solenuk.todotaskspetproject.dtos.request.CreateTaskDTO;
+import com.solenuk.todotaskspetproject.dtos.response.ResponseTaskDTO;
+import com.solenuk.todotaskspetproject.entities.Task;
+import com.solenuk.todotaskspetproject.enums.TaskState;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapper {
+    public Task toEntity(CreateTaskDTO createTaskRequest) {
+        return Task.builder()
+            .title(createTaskRequest.title())
+            .description(createTaskRequest.description())
+            .priority(createTaskRequest.priority())
+            .state(TaskState.NEW)
+            .creatorId(createTaskRequest.creatorId())
+            .build();
+    }
+
+    public ResponseTaskDTO toResponse(Task task) {
+        return new ResponseTaskDTO(
+            task.getId(),
+            task.getTitle(),
+            task.getDescription(),
+            task.getState(),
+            task.getPriority(),
+            task.getCreatorId(),
+            task.getCreatedAt(),
+            task.getUpdatedAt()
+        );
+    }
+}
