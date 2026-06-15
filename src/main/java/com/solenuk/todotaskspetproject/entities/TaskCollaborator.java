abb8c2838f065d4f3a370c8d227506aac6f0b18d
Ihor Selianyk <iseljani@gmail.com>
2026-06-15 17:15:57 +0300

    Created entity for task collaborators

---
+++ src/main/java/com/solenuk/todotaskspetproject/entities/TaskCollaborator.java
@@ -0,0 +1,28 @@
+package com.solenuk.todotaskspetproject.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "task_collaborators")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskCollaborator {
+    @EmbeddedId
+    private TaskCollaboratorId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("taskId")
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    public TaskCollaborator(Task task, Integer userId) {
+        this.task = task;
+        this.id = new TaskCollaboratorId(task.getId(), userId);
+    }
+
+    public Integer getUserId() {
+        return this.id != null ? this.id.getUserId() : null;
+    }
+}
