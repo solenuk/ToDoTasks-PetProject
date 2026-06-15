5402dd9da75506111196b24a5868df15d981010f
Ihor Selianyk <iseljani@gmail.com>
2026-06-15 18:16:08 +0300

    Added new methods to task repository to work with collaborators

--- src/main/java/com/solenuk/todotaskspetproject/repositories/TaskRepository.java
+++ src/main/java/com/solenuk/todotaskspetproject/repositories/TaskRepository.java
@@ -3,6 +3,8 @@ package com.solenuk.todotaskspetproject.repositories;
 import com.solenuk.todotaskspetproject.entities.Task;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -22,4 +24,28 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
      * @return true if a user has created some tasks, false otherwise
      */
     boolean existsByCreatorId(Integer creatorId);
+
+    /**
+     * Retrieves all tasks that a user has access to, either as the creator or as a collaborator.
+     *
+     * @param userId the id of the user; must not be null
+     * @return a list of Tasks accessible by the user
+     */
+    @Query(
+        "SELECT DISTINCT t FROM Task t LEFT JOIN t.collaborators tc WHERE t.creatorId = :userId OR tc.id.userId = "
+            + ":userId")
+    List<Task> findAllTasksForUser(@Param("userId") Integer userId);
+
+    /**
+     * Checks whether a user has access to a specific task, i.e., the user is either the creator
+     * or a collaborator of that task.
+     *
+     * @param taskId the id of the task to check; must not be null
+     * @param userId the id of the user to verify access for; must not be null
+     * @return true if the user has access to the task, false otherwise
+     */
+    @Query(
+        "SELECT COUNT(t) > 0 FROM Task t LEFT JOIN t.collaborators tc WHERE t.id = :taskId AND (t.creatorId = :userId"
+            + " OR tc.id.userId = :userId)")
+    boolean hasAccessToTask(@Param("taskId") Integer taskId, @Param("userId") Integer userId);
 }
