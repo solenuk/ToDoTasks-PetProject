2ad94cb01d6b14d3a8161631a82424ad433892a6
Ihor Selianyk <166725875+solenuk@users.noreply.github.com>
2026-06-19 15:29:57 +0300

    Fixed: added auth privider to security filter chain (#59)

--- src/main/java/com/solenuk/todotaskspetproject/security/SecurityConfig.java
+++ src/main/java/com/solenuk/todotaskspetproject/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
+            .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
