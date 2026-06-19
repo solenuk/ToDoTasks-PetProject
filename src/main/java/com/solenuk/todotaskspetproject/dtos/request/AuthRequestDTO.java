0caf354faf50759429b01e0016e71986a903fb7d
Ihor Selianyk <iseljani@gmail.com>
2026-06-19 15:49:12 +0300

    Fixed names for auth dtos

--- src/main/java/com/solenuk/todotaskspetproject/dtos/request/AuthRequestDTO.java
+++ src/main/java/com/solenuk/todotaskspetproject/dtos/request/AuthRequestDTO.java
@@ -3,7 +3,7 @@ package com.solenuk.todotaskspetproject.dtos.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record AuthRequest(
+public record AuthRequestDTO(
     @NotBlank(message = "Email is required!")
     @Email(message = "Must be a valid email format")
     String email,
--- src/main/java/com/solenuk/todotaskspetproject/dtos/response/AuthResponseDTO.java
+++ src/main/java/com/solenuk/todotaskspetproject/dtos/response/AuthResponseDTO.java
@@ -1,6 +1,6 @@
 package com.solenuk.todotaskspetproject.dtos.response;
 
-public record AuthResponse(
+public record AuthResponseDTO(
     String token
 ) {
 }
