b382189126d51dce854ef1c52d8b33c9cfbba528
Ihor Selianyk <166725875+solenuk@users.noreply.github.com>
2026-06-22 14:39:57 +0300

    Added SecurityUser as common entity for spring security context (#108)

---
+++ src/main/java/com/solenuk/todotaskspetproject/common/enteties/SecurityUser.java
@@ -0,0 +1,51 @@
+package com.solenuk.todotaskspetproject.common.enteties;
+
+import java.util.Collection;
+import lombok.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public record SecurityUser(
+    Integer id,
+    String email,
+    String password,
+    Collection<? extends GrantedAuthority> authorities
+) implements UserDetails {
+    @Override
+    @NonNull
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    @NonNull
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
