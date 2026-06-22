a53566152f4b0cc5ae3f78ea76b5d5489db0cdf6
Ihor Selianyk <166725875+solenuk@users.noreply.github.com>
2026-06-22 16:12:37 +0300

    Fixed the missing 'final' keyword UserDetailsServiceImpl (#117)

--- src/main/java/com/solenuk/todotaskspetproject/user/services/UserDetailsServiceImpl.java
+++ src/main/java/com/solenuk/todotaskspetproject/user/services/UserDetailsServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     @NonNull
